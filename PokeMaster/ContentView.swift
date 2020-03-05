026c40c2c2e2e679d4085b91c8498d112931548a
Dong <donly@magicalboy.com>
2020-03-06 00:40:41 +0800

    starter

---
+++ PokeMaster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PokeMaster
+//
+//  Created by Dong on 6/3/2020.
+//  Copyright © 2020 IOSEXAMPLE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
